70c057b77784882909bddf88abb1753ee677c232
Kola771 <koladeaboudou@gmail.com>
2024-06-14 11:12:33 +0200

    fortify installé et configuré

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,6 +21,10 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
+                if($request->expectsJson())
+                {
+                    return response()->json(["Déjà authentifié"], 200);
+                }
                 return redirect(RouteServiceProvider::HOME);
             }
         }
